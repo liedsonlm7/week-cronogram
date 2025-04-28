4e43e5b1fb7ff842e469bbe034b7cbf8146e4f4a
liedsonlm7 <liedsonlm7@gmail.com>
2025-04-28 19:55:37 -0300

    week-cronogram

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const cronograma = {
+    segunda: [
+      "8h-12h: Estudar React.js, TypeScript e NodeJS",
+      "12h-13h: Almoço",
+      "13h-14h: Estudar Banco de Dados (SQL Server) e Redes",
+      "14h-17h: Estudos de Idiomas (Inglês e Espanhol)",
+      "19h-22h: Material da faculdade / Aulas online"
+    ],
+    terca: [
+      "8h-12h: Estudar C# e Banco de Dados (PostgreSQL)",
+      "12h-13h: Almoço",
+      "14h-17h: Estudos de Idiomas (Inglês e Espanhol)",
+      "19h-22h: Material da faculdade / Aulas online"
+    ],
+    quarta: [
+      "8h-12h: Estudar React.js, TypeScript e NodeJS",
+      "12h-13h: Almoço",
+      "13h-14h: Estudar Banco de Dados (SQL Server) e Redes",
+      "14h-17h: Estudos de Idiomas (Inglês e Espanhol)",
+      "19h-22h: Material da faculdade / Aulas online"
+    ],
+    quinta: [
+      "8h-12h: Estudar C# e Banco de Dados (PostgreSQL)",
+      "12h-13h: Almoço",
+      "14h-17h: Estudos de Idiomas (Inglês e Espanhol)",
+      "19h-22h: Material da faculdade / Aulas online"
+    ],
+    sexta: [
+      "8h-12h: Estudar React.js, TypeScript e NodeJS",
+      "12h-13h: Almoço",
+      "13h-14h: Estudar Banco de Dados (SQL Server) e Redes",
+      "14h-17h: Estudos de Idiomas (Inglês e Espanhol)",
+      "19h-22h: Material da faculdade / Aulas online"
+    ],
+    sabado: [
+      "Revisar conteúdos estudados",
+      "Estudar Alemão",
+      "Curso de AWS"
+    ]
+  };
+
+function mostrarDia(dia) {
+    const conteudo = document.getElementById("conteudo");  
+    conteudo.innerHTML = "";
+    if (cronograma[dia]) {
+        const lista = document.createElement('ul');
+        cronograma[dia].forEach(atividade => {
+            const item = document.createElement('li');
+            item.textContent = atividade;
+            lista.appendChild(item);
+        });
+        conteudo.appendChild(lista);
+    }
+}
+
+const botaoTema = document.getElementById('toggle-theme');
+botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
